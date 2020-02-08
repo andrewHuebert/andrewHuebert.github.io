4dc8fd0b123dfa01882ce7ec0597cb0366f8956b
Andrew Huebert <54640327+andrewHuebert@users.noreply.github.com>
2020-02-07 19:13:55 -0800

    Update clean-blog.js

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1,6 +1,5 @@
 (function($) {
   "use strict"; // Start of use strict
-
   
   // Floating label headings for the contact form
   $("body").on("input propertychange", ".floating-label-form-group", function(e) {
